4edacd5d4b566c36fecdcdaa63ecf74ca3df2abd
yourdeadprince <147982146+yourdeadprince@users.noreply.github.com>
2023-12-14 14:18:47 +0100

    snipy.sh

--- snipy.sh
+++ snipy.sh
@@ -3,6 +3,6 @@
 for (( i=8; i<18; i++));
 do
     echo "pracuję nad p$i"
-    ./freebayes -f $HOME/genome/scer.fa $HOME/seq/tagi/p$i*bwa-markdup.bam >>var.vcf
+    ./freebayes -f $HOME/genome/scer.fa $HOME/seq/tagi/p$i*bwa-markdup.bam >snipy_p$i.vcf
 done
 echo "done :)"
