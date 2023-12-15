43e05331b605800534dc62e153adbcdb4ff372ad
kasiatom <katarzyna.tomala@uj.edu.pl>
2023-12-15 11:59:20 +0100

    changes kt

--- free.sh
+++ free.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 REF="$HOME/genome/scer.fa"
 
+## trzeba zaktywować środowisko bio
+
 for ba in $HOME/wyniki/p*.bam
 do
+name=$(basename "$ba")
+
 ./freebayes \
  -f $REF \
  -C 5 \
@@ -10,11 +14,9 @@ do
  -L wyniki \
  -p 1 \
  --report-genotype-likelihood-ma \
- --haplotype-length 0 \
  --min-alternate-count 2 \
  --min-alternate-fraction 0.2 \
- --pooled-continuous \
- --report-monomorphic \
- "$ba" |  vcffilter -f "QUAL > 20" > $HOME/wyniki2
+ --report-monomorphic \  ## byc może trzeba bedzie wyrzucić
+ "$ba" | bcftools filter -i  "QUAL > 20" > $HOME/wyniki2/"$name".vcf
 done
 echo wyszlo
