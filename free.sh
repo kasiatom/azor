92b5939bd53eaef22f8c0fcb84485e08a7ab72b3
kasiatom <katarzyna.tomala@uj.edu.pl>
2023-12-18 17:49:19 +0100

    rm spaces

--- free.sh
+++ free.sh
@@ -9,14 +9,14 @@ name=$(basename "$ba")
 
 ./freebayes \
  -f $REF \
- -C 5 \
- -g 1000 \ 
+  -C 5 \
+ -g 1000 \
  -L wyniki \
  -p 1 \
  --report-genotype-likelihood-ma \
  --min-alternate-count 2 \
  --min-alternate-fraction 0.2 \
- --report-monomorphic \  ## byc może trzeba bedzie wyrzucić
+ --report-monomorphic \
  "$ba" | bcftools filter -i  "QUAL > 20" > $HOME/wyniki2/"$name".vcf
 done
 echo wyszlo
