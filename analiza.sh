f87536122b9ce61efe3a7925f3c2cb7f15418376
kasiatom <katarzyna.tomala@uj.edu.pl>
2023-12-18 09:50:23 +0100

    kt check

--- analiza.sh
+++ analiza.sh
@@ -3,6 +3,9 @@
 for (( i=8; i<18; i++));
 do
     echo "pracuję nad p$i"
-    bcftools norm -m -any -f $HOME/genome/scer.fa  snipy_p$i.vcf | bcftools filter -e "QUAL<20" | bcftools filter -e "FORMAT/AO < 5" | bcftools filter -e  "FMT/DP < 10 & FMT/DP > 400" | bcftools filter -o analiza_p$i.vcf
+    bcftools norm -m -any -f $HOME/genome/scer.fa  snipy_p$i.vcf | \
+    bcftools filter -e "QUAL<20" | \
+    bcftools filter -e "FORMAT/AO < 5" |\
+    bcftools filter -e  "FORMAT/DP < 10 | FORMAT/DP > 400" -o analiza_p$i.vcf ## lub nie i, bo to warunek rozłżczny, poprawiłam też FMT na FORMAT => kod powinien byc przejrzysty, trzymamy sie więc jednej konwencji 
 done
 echo "done :)"
