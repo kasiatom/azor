c8a3c9111906247344aeb8052df9864122d9cab9
kasiatom <katarzyna.tomala@uj.edu.pl>
2023-12-18 09:56:42 +0100

    kt check

--- analiza.sh
+++ analiza.sh
@@ -6,6 +6,6 @@ do
     bcftools norm -m -any -f $HOME/genome/scer.fa  snipy_p$i.vcf | \
     bcftools filter -e "QUAL<20" | \
     bcftools filter -e "FORMAT/AO < 5" |\
-    bcftools filter -e  "FORMAT/DP < 10 | FORMAT/DP > 400" -o analiza_p$i.vcf ## lub nie i, bo to warunek rozłżczny, poprawiłam też FMT na FORMAT => kod powinien byc przejrzysty, trzymamy sie więc jednej konwencji 
+    bcftools filter -e  "FORMAT/DP < 10 | FORMAT/DP > 400" -o analiza_p$i.vcf ## lub nie i, bo to warunek rozłączny, poprawiłam też FMT na FORMAT => kod powinien byc przejrzysty, trzymamy sie więc jednej konwencji 
 done
 echo "done :)"
