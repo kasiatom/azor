dc89ce0afbb3cbd09e9a12f36300d2cbf5c1fc3f
KarolinaKoronska <147943537+KarolinaKoronska@users.noreply.github.com>
2023-12-19 10:12:14 +0100

    Update norma.sh

--- norma.sh
+++ norma.sh
@@ -5,17 +5,17 @@ REF="$HOME/genome/scer.fa"
 
 for p in $HOME/wyniki2/p*.vcf
 do
-bcftools norm -m -any -f $REF -o $p -O z
+bcftools norm -m -any -f $REF -o $HOME/wyniki2/p*_2.vcf.gz -O z
 echo zrobiono 
 done
-bcftools merge $HOME/wyniki2/p*.vcf.gz
+bcftools merge $HOME/wyniki2/p*_2.vcf.gz -o $HOME/wyniki3/pr.vcf.gz
 
 vep --cache --offline --format vcf --vcf --force_overwrite \
-    --dir_cache $HOME/wyniki2/ \
-    --input_file $HOME/wyniki2/p*.vcf.gz \
+    --dir_cache $HOME/wyniki3/ \
+    --input_file $HOME/wyniki3/pr.vcf.gz \
     --species saccharomyces_cerevisiae \
     --compress_output bgzip \
     --distance 200 \
     --no_intergenic \
     --force_overwrite \
-    --output_file $HOME/wyniki2/pro.vcf.gz
+    --output_file $HOME/wyniki3/pro.vcf.gz
