096cd619da1abc3f65357a9767e554995fefac7a
yourdeadprince <147982146+yourdeadprince@users.noreply.github.com>
2023-12-29 12:43:21 +0100

    Analiza.sh
    Filters + tabelka

--- analiza.sh
+++ analiza.sh
@@ -8,4 +8,12 @@ do
     bcftools filter -e "FORMAT/AO < 5" |\
     bcftools filter -e  "FORMAT/DP < 10 | FORMAT/DP > 400" -o analiza_p$i.vcf ## lub nie i, bo to warunek rozłączny, poprawiłam też FMT na FORMAT => kod powinien byc przejrzysty, trzymamy sie więc jednej konwencji 
 done
+
+for (( i=8; i<18; i++));
+do
+    echo "analizuję p$i"
+    bgzip analiza_p$i.vcf
+    bcftools query -f '%CHROM\t,%POS\t,%REF\t,%ALT\t,[%DP]\t,[%AO]\n' $HOME/azor/analiza/analiza_p$i.vcf.gz >> tabelka.csv
+done
+
 echo "done :)"
